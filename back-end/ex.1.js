f98380ad043ef0bc73b8de5a7b1d8d480ac8e4a3
diegoaroucha-droid <diego.aroucha@aluno.educa.go.gov.br>
2026-02-10 10:47:58 -0300

    Update ex.1.js

--- back-end/ex.1.js
+++ back-end/ex.1.js
@@ -1,21 +1,30 @@
 **1.** O que é o Node.js?
-
-**2.** O Node.js usa qual linguagem de programação?
-
+é um ambiente de tempo de execução (runtime environment) de código aberto e multiplataforma que permite executar JavaScript no lado do servidor, fora de um navegador.
+  
+2. O Node.js usa qual linguagem de programação?
+JavaScript
+  
 **3.** O Node.js roda no navegador ou fora dele?
+O Node.js roda fora do navegador (servidor ou máquina local). 
 
 **4.** Para que serve o Node.js?
-
-**5.** Qual comando é usado para executar um arquivo Node.js no terminal?
+serve principalmente para executar código JavaScript no lado do servidor (backend), 
+  
+5. Qual comando é usado para executar um arquivo Node.js no terminal?
+node app.js
 
 **6.** O que é o **npm**?
-
+O npm (Node Package Manager) é o gerenciador de pacotes padrão para o ambiente de execução Node.js e o maior registro de software de código aberto do mundo
+  
 **7.** O que significa a palavra **módulo** no Node.js?
-
+No Node.js, um módulo é uma unidade de código encapsulada — geralmente um arquivo JavaScript ou uma pasta contendo um arquivo index.js — que organiza, separa e reutiliza funcionalidades dentro de uma aplicação. 
+  
 **8.** O Node.js é mais usado no **front-end** ou no **back-end**?
-
+back-end.
+  
 **9.** O que é o arquivo **package.json**?
+O arquivo package.json é o coração de qualquer projeto Node.js ou aplicação JavaScript moderna (como React, Vue, etc.). Ele funciona como um "manifesto", "certidão de nascimento" ou um arquivo de configuração central que contém metadados relevantes sobre o projeto e gerencia suas dependências. 
 
 **10.** O Node.js é gratuito ou pago?
-
+gratuito
 ---
